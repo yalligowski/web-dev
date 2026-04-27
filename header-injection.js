a48c6aaded10958c03529b6ab66dcd47d95d487b
yalligowski <student.el@youngsvilleacademy.org>
2026-04-27 08:28:38 -0400

    Create header-injection.js

---
+++ header-injection.js
@@ -0,0 +1,8 @@
+let elem = document.createElement("div");
+elem.innerHTML="<link rel='stylesheet' href='https://yalligowski.github.io/web-dev/header.css'></link>"
+document.body.prepend(elem);
+
+fetch("https://yalligowski.github.io/web-dev/globalheader.html")
+  .then((output) => output.text())
+  .then((text) => {elem.innerHTML = text;})
+  .then((e) => console.error());
